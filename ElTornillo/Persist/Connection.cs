eeedfa500f518d2862e79330d621d9c03d780279
Tomy0032 <90046079+Tomy0032@users.noreply.github.com>
2024-05-20 19:35:42 -0300

    Connection and basic query

---
+++ ElTornillo/Persist/Connection.cs
@@ -0,0 +1,40 @@
+﻿
+using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persist
+{
+    public class Connection
+    {
+        private MySqlConnection connection;
+        private string server = "localhost";
+        private string database = "pruebas"; // Cambiar a base de datos real
+        private string user = "root";
+        private string password = "contraseña";// Cambiar a contraseña real
+        private string connectionString;
+
+        public Connection()
+        {
+            connectionString = $"Server={server};" +
+                $"Database={database};" +
+                $"User Id={user};" +
+                $"Password={password};" +
+                $"SslMode=None;";
+        }
+
+        public MySqlConnection getConnection()
+        {
+            if (connection == null)
+            {
+                connection = new MySqlConnection(connectionString);
+                connection.Open();
+            }
+
+            return connection;
+        }
+    }
+}
---
+++ ElTornillo/Persist/Queries.cs
@@ -0,0 +1,45 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persist
+{
+    public class Queries
+    {
+        private Persist.Connection connection;
+
+        public Queries(Persist.Connection connection)
+        {
+            this.connection = connection;
+        }
+
+        public string GetData()
+        {
+            string result = "";
+            MySqlDataReader mySqlDataReader = null;
+            string query = "select * from test"; // Cambiar 'test' por la tabla de l que se quiera sacar información.
+
+            if (connection.getConnection() != null)
+            {
+                MySqlCommand mySqlCommand = new MySqlCommand(query);
+                mySqlCommand.Connection = connection.getConnection();
+                mySqlDataReader = mySqlCommand.ExecuteReader();
+
+                while (mySqlDataReader.Read())
+                {
+                    result += mySqlDataReader.GetString("nombre") + "\n"; // Cambiar 'nombre' por la columna deseada.
+                }
+                mySqlDataReader.Close();
+            }
+            else
+            {
+                result = "Error al conectar.";
+            }
+
+            return result;
+        }
+    }
+}
